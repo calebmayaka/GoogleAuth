29ac2f88cbc56da669c26b7759cf034acd81845d
Caleb Mayaka <mayakaombogo254@gmail.com>
2025-02-10 23:06:35 +0300

    update

--- G/main/urls.py
+++ G/main/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='homepage'),
+    path('login/',views.login, name='loginpage'),
+    path('signup/',views.signup, name='signupage'),
+    path('base/',views.base,name='basetemplate'),
+    path('about/',views.about, name='aboutpage'),
+    path('contactus/',views.contact, name='contactus')
 ]
--- G/main/views.py
+++ G/main/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, 'main/home.html')
 
+def login(request):
+    return render(request,'main/login.html')
+
+def signup(request):
+    return render(request,'main/signup.html')
+
+def base(request):
+    return render(request,'main/base.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
     
     
